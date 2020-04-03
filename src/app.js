62ddc957eb74dfd668b31dea5db701a6efffc52b
7heWorrior <manas.mahajan17@vit.edu>
2020-04-03 13:16:47 +0530

    second basic commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+//console.log('this is starting of Course Project')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+
+// console.log(__dirname)
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(publicPath))
+
+
+
+app.get('',(req,res)=>{
+    res.render('index')
+})
+
+
+app.listen(3000, () =>{
+    console.log('Server is up on port 3000')
+})
